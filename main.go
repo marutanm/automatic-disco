6596694ed33c6f8bae4bb212f346c3ae3278b891
marutanm <marutanm@users.noreply.github.com>
2016-10-19 12:19:49 +0900

    define field name

--- main.go
+++ main.go
@@ -6,15 +6,15 @@ import (
 )
 
 type asset struct {
-	fileName string
-	md5      string
+	FileName string `json:"filename"`
+	Md5      string `json:"md5"`
 }
 
 func main() {
 	assets := []asset{
-		asset{"Images/background.jpg", "..."},
-		asset{"Images/icon.png", "..."},
-		asset{"Images/button.png", "..."},
+		asset{"Images/background.jpg", "bg"},
+		asset{"Images/icon.png", "icn"},
+		asset{"Images/button.png", "btn"},
 	}
 	fmt.Println(assets)
 	assetsB, _ := json.Marshal(assets)
